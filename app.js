8e10d8437ef337a67cf39c26a07fecdb03837cb4
khainq2305 <khainqpc08388@gmail.com>
2024-05-08 20:54:50 +0700

    init text

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(473439646369464);
+console.log('473439646369464');
+
+const bien1 = 3;
+if (bien1 > 3) {
+console.log('gia tri nay lon hon 3');
+} else {
+    console.log('gia tri nay nho hon 3'[]);
+
+}
+
